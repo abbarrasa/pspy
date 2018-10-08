8fb9a0c0fcefb6ad81de617989bd583d65149622
Alberto Buitrago Barrasa <abbarrasa@gmail.com>
2018-10-08 11:25:42 +0200

    Update game.py

--- pspy/game.py
+++ pspy/game.py
@@ -1,5 +1,8 @@
+from sqlobject import *
+
+
 class Game(SQLObject):
-  title = StringCol(length=255)
+  title = StringCol(length=255, unique=True)
   description = StringCol()
   type = EnumCol(['ISO', 'CSO'])
   path = StringCol()
