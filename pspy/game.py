b25af7e4b0e8f32b926b67a99c1710d9e9873d7b
Alberto Buitrago <abbarrasa@gmail.com>
2018-10-09 00:25:32 +0200

    Reordering classes

--- pspy/game.py
+++
@@ -1,8 +0,0 @@
-from sqlobject import *
-
-
-class Game(SQLObject):
-  title = StringCol(length=255, unique=True)
-  description = StringCol()
-  type = EnumCol(['ISO', 'CSO'])
-  path = StringCol()
