e8964871305905788c28b71f8fcaea2b364790b4
Alberto Buitrago <abuitrago@estrategiasdeinversion.com>
2018-10-15 12:10:14 +0200

    Settings

--- pspy/database.py
+++ pspy/database.py
@@ -4,17 +4,17 @@ import os
 
 
 class Database(SQLObject):
-	def connect(db_filename):
-		path_to_db = os.path.abspath(db_filename)
+	def connect(self, db_filename):
+		path_to_db = os.path.expanduser(db_filename)
 		uri = 'sqlite:{0}'.format(path_to_db)
 		#Create and open connection to a database file.
 		sqlhub.processConnection = connectionForURI(uri)
 
-	def createTables():
+	def createTables(self):
 		for entity in (Game):
 			entity.createTable(ifNotExists=True)
 
-	def close():
+	def close(self):
 		#close connection
 		sqlhub.processConnection.close()
 
