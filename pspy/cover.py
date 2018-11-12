d86478d54b03f8b6821876215ebaa7db584ff06b
Alberto Buitrago Barrasa <abbarrasa@gmail.com>
2018-11-12 14:25:35 +0100

    Update cover.py

--- pspy/cover.py
+++ pspy/cover.py
@@ -8,38 +8,32 @@ import resources
 class Cover(object):
     im = None
 
-    def __init__(self, filename=None, raw=None):
-        print("Vamos a guardar filename")
+    def __init__(self, filename):
+        print("Vamos a guardar filename: " + filename)
         if filename:
             if filename.find('://') > 0:
                 im = Image.open(urlopen(filename))
             else:
                 im = Image.open(filename)
                 im.thumbnail((326, 559), Image.ANTIALIAS)
-        elif raw:
-            im = Image.frombytes('RGBA', (326, 559), raw)
         else:
-            raise ValueError("File name or raw bytes pictures is required")
+            raise ValueError("File name of picture is required")
 
         self.im = im
 
-    def save(self, outfile, directory):
-        filename = os.path.join(directory, outfile + ".png")
-        self.im.save(filename, 'PNG')
-
-    def raw(self):
-        output = io.BytesIO()
-        self.im.save(output, format='PNG')
-        return output.getvalue()
-
     def generateThumbnail(self, outfile, directory):
         background = Image.new('RGBA', (342, 582), (255, 0, 0, 0))
+        print("Create background")
         background.paste(self.im, (2, 11), self.im)
+        print("Paste background")
         foreground = Image.open(":/resources/templates/psp-game-cover-template.png")
         background.paste(foreground, (0, 0), foreground)
+        print("Paste foreground")
         background.show()
+        print("Show")
         try:
             filename = os.path.join(directory, outfile + ".png")
             background.save(filename, 'PNG')
+            print("Saving")
         except IOError:
             print("Cannot create thumbnail")
